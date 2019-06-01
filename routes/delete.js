dc8cc72057200c54768b78a0159e7483b16be598
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-06-01 18:51:15 -0500

    updateOne => update

--- routes/delete.js
+++ routes/delete.js
@@ -11,42 +11,43 @@ const database = require('../database.js')
     if (password != 'BIG_TING_BADADDADA'){
       res.status(404).end();
     }
-    database.connect(db=>{
-      //delete a gig
-      if (mode=='gigs'){
-        db.db('gigs').collection('gigs').drop();
-        res.status(200).send("Deleted gigs");
-        db.close();
-      }
-      //delete a band
-      if (mode=='bands'){
-        db.db('bands').collection('bands').drop();
-        res.status(200).send("Deleted bands");
-        db.close();
-      }
-      //delete an account
-      if (mode=='accounts'){
-        db.db('users').collection('stripe_users').drop();
-        res.status(200).send("Deleted user_stripe_accounts");
-        db.close();
-      }
-      //delete a customer
-      if (mode=='customers'){
-        db.db('users').collection('stripe_customers').drop();
-        res.status(200).send("Deleted stripe_customers");
-        db.close();
-      }
-      //invalid request
-      else{
-        res.status(401).end();
-        db.close();
-      }
-
-    }, err=>{
-      //error case
-      console.log("THERE WAS AN ERROR DELete EVERYTHING : " + err);
-      res.status(500).end();
-    });
+    else{
+      database.connect(db=>{
+        //delete a gig
+        if (mode=='gigs'){
+          db.db('gigs').collection('gigs').drop();
+          res.status(200).send("Deleted gigs");
+          db.close();
+        }
+        //delete a band
+        if (mode=='bands'){
+          db.db('bands').collection('bands').drop();
+          res.status(200).send("Deleted bands");
+          db.close();
+        }
+        //delete an account
+        if (mode=='accounts'){
+          db.db('users').collection('stripe_users').drop();
+          res.status(200).send("Deleted user_stripe_accounts");
+          db.close();
+        }
+        //delete a customer
+        if (mode=='customers'){
+          db.db('users').collection('stripe_customers').drop();
+          res.status(200).send("Deleted stripe_customers");
+          db.close();
+        }
+        //invalid request
+        else{
+          res.status(401).end();
+          db.close();
+        }
+      }, err=>{
+        //error case
+        console.log("THERE WAS AN ERROR DELete EVERYTHING : " + err);
+        res.status(500).end();
+      });
+    }
   });
 
   //for deleteing user created objects
